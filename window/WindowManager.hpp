57554ca747948003a76617e3ae44e379d141017e
Sebastian Luzynski <sebastian.luzynski@gmail.com>
2022-04-03 23:40:25 +0200

    Add WindowManager

---
+++ window/WindowManager.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+class WindowManager {
+  public:
+    void run();
+
+  private:
+    void initWindow();
+    void initVulkan();
+    void createInstance();
+    void mainLoop();
+    void cleanup();
+
+    GLFWwindow *window;
+    VkInstance instance;
+};
